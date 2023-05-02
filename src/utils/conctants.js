3a5b3deda5df90dfc95f920cbe5044bbaba6041e
Askar1o <Gilmanov.Ask@yandex.ru>
2023-05-02 17:47:29 +0500

    9 проект

--- src/utils/conctants.js
+++ src/utils/conctants.js
@@ -11,10 +11,10 @@ export const popupOpenButtonElement = document.querySelector('.profile__edit-but
 export const popupAddButtonOpen = document.querySelector('.profile__add-button');
 export const popupButtonSave = document.querySelector('.popup__button-save');
   
-export const formElementProfile = document.querySelector('.popup__form');
+export const formElementProfile = document.querySelector('.popup__form_profile');
 export const nameInput = document.querySelector('.popup__type_input_username');
 export const jobInput = document.querySelector('.popup__type_input_job');
-export const formPopupPlace = document.querySelector('.popup__form');
+export const formPopupPlace = document.querySelector('.popup__form_place');
 export const buttonEditAvatar = document.querySelector('.profile__avatar-button');
-export const formEditAvatar = document.querySelector('.popup__form');
+export const formEditAvatar = document.querySelector('.popup__form_update');
 export const avatar = document.querySelector('.profile__avatar');
